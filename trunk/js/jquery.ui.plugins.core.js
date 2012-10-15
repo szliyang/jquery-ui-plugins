ab13c6dbd03308d14380f1ef31473e8108ec6079
chad.lavigne@gmail.com <chad.lavigne@gmail.com@425839f6-d57b-9616-3b18-1f48c6fb0bc5>
2012-10-15 16:10:48 +0000

    Add core lib to store common code.

---
+++ trunk/js/jquery.ui.plugins.core.js
@@ -0,0 +1,12 @@
+;(function($, undefined ) {
+	
+	$.extend($.ui.keyCode, {
+		A: 65,
+		NUM_LOCK: 144,					
+		NUMPAD_ZERO: 96,		
+		PAUSE_BREAK: 19,		
+		SCROLL_LOCK: 145,
+		WINDOWS_RIGHT: 92
+	});
+	
+})(jQuery);
