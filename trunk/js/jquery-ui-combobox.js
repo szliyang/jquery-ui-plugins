caf53a3fe81109d21125d88a3a7c91637d789f02
chad.lavigne@gmail.com <chad.lavigne@gmail.com@425839f6-d57b-9616-3b18-1f48c6fb0bc5>
2012-10-24 20:07:11 +0000

    Add some options, methods and events to combobox.

--- trunk/js/jquery-ui-combobox.js
+++ trunk/js/jquery-ui-combobox.js
@@ -1,17 +1,34 @@
+/*
+ * jQuery UI Combobox 0.0.9
+ *
+ * Copyright 2012, Chad LaVigne
+ * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php) 
+ *
+ * http://code.google.com/p/jquery-ui-plugins/wiki/Combobox
+ *
+ * Depends:
+ *  jquery 1.8.2
+ *	jquery.ui.core.js
+ *	jquery.ui.widget.js
+ */
 $.widget("uiplugins.combobox", {
-	options: {		
+	options: {
+		"buttonClass": "",
+		"buttonStyle": {},
+		"height": 20,
+		"ignoreCase": true,
+		"inputClass": "",
+		"inputStyle": {},
 		"maxHeight": 200,
-		"matchValue": true, // TODO: implement this, determines if the autocomplete searches on option values or the option text,
-		"containsSearch": true // TODO: implement this, option to specify if we should do a contains search or just a starts with
+		"searchType": "startsWith", // specify what type of search the auto complete does, valid values are contains or startsWith
+		"width": 200
 	},
 	_create: function() {
-		var self = this;		
-		// wrap everything in a div, the elements are floated to make the tops align but we don't want the widget floated
-		var select = this.element.hide(),
-			selected = select.children(":selected"),
+		var self = this;				
+		var $select = this.element.addClass("ui-combobox-select").hide(),
+			selected = $select.children(":selected"),
 			value = selected.val() ? selected.text() : "";
-		this.element.addClass("ui-combobox-select");
-		var $input = $("<input />")
+		var $input = this.$input = $("<input />")
 			.keydown(function(event) {
 				var $this = $(this);
 				// if they hit arrow down and the list isn't showing, show the whole thing regardless of the current value
@@ -19,31 +36,36 @@ $.widget("uiplugins.combobox", {
 					$this.autocomplete("search", "");
 					return false;
 				}
-			})
-			.addClass('ui-combobox-input')
-			.insertAfter(select)
+			})			
+			.height(this.options.height)
+			.insertAfter($select)
 			.val(value)
+			// wrap elements in a div because they're floated to make the tops align but we don't want the widget floated
 			.wrap($("<div></div>").addClass("ui-combobox"))
 			.autocomplete({
 				delay: 0,
 				minLength: 0,
 				source: function(request, response) {
-					var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
-					response(select.children("option").map(function() {
+					var quantifier = self.options.searchType == "startsWith" ? "^" : "";					
+					var modifier = self.options.ignoreCase && self.options.ignoreCase !== "false" ? "i" : "";					
+					var matcher = new RegExp(quantifier + $.ui.autocomplete.escapeRegex(request.term), modifier);
+					
+					response($select.children("option").map(function() {
 						var text = $(this).text();
 						if (this.value && (!request.term || matcher.test(text))) {
 							return {
+								// this regex highlights the matching part of the label
 								label: text.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + $.ui.autocomplete.escapeRegex(request.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>"),
 								value: text,
 								option: this
 							};
 						}
-					}));
+					}));					
 				},
 				select: function( event, ui ) {
 					ui.item.option.selected = true;
-					self._trigger("selected", event, {
-						item: ui.item.option
+					self._trigger("select", event, {
+						"item": ui.item.option
 					});
 				},
 				change: function(event, ui) {
@@ -51,24 +73,33 @@ $.widget("uiplugins.combobox", {
 						// check the contents of the text field and see if it matches the text of any options
 						var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex($(this).val()) + "$", "i");
 						var valid = false;
-
-						select.children("option").each(function() {							
-							if (this.text.match(matcher)) { // we're matching on option text here instead of value, need to add an option to specify which to match on so the user can do either
+						
+						$select.children("option").each(function() {							
+							if (this.text.match(matcher)) {
 								this.selected = valid = true;
+								$select.val(this.value);
+								self._trigger("change", event, {"item": this});
 								return false;
 							}
 						});
 						
 						if (!valid) {
-							// remove invalid value, as it didn't match anything
-							$(this).val("");
-							select.val("");
+							// remove invalid value that didn't match anything, just set the value back to what it was							
+							$(this).val($select.children("option:selected").text());
 							return false;
 						}
 					}
+				},
+				open: function(event, ui) {					
+					self._trigger("open");					
+				},
+				close: function() {
+					self._trigger("close");
 				}
 			})
-			.addClass("ui-widget ui-widget-content ui-corner-left");
+			.addClass("ui-widget ui-widget-content ui-corner-left ui-combobox-input" + (this.options.inputClass ? " " + this.options.inputClass : ""))
+			.css(this.options.inputStyle);
+		
 		
 		$input.keydown(function(event) {
 			var key = event.which;
@@ -87,7 +118,7 @@ $.widget("uiplugins.combobox", {
 				.appendTo(ul);
 		};				   
 		
-		$("<button>&nbsp;</button>")
+		var $button = this.$button = $("<button>&nbsp;</button>")
 			.attr("tabIndex", -1)
 			.attr("title", "Show All Items")
 			.insertAfter($input)
@@ -98,8 +129,9 @@ $.widget("uiplugins.combobox", {
 				text: false
 			})									
 			.removeClass("ui-corner-all")
-			.addClass("ui-corner-right ui-button-icon ui-combobox-button")
-			.height($input.height() + Number($input.css("border-top-width").replace("px", "")) + Number($input.css("border-bottom-width").replace("px", "")))
+			.addClass("ui-corner-right ui-button-icon ui-combobox-button" + (this.options.buttonClass ? " " + this.options.buttonClass : ""))
+			.css(this.options.buttonStyle)
+			.height(this._getButtonHeight())
 			.mousedown(function() {
 				// close if already visible				
 				if ($input.autocomplete("widget").is(":visible")) {
@@ -111,6 +143,66 @@ $.widget("uiplugins.combobox", {
 				return false;
 			});
 		
+		$input.width(this.options.width - $button.width());			
+	},
+	_getButtonHeight: function() {
+		return this.$input.height() + Number(this.$input.css("border-top-width").replace("px", "")) + Number(this.$input.css("border-bottom-width").replace("px", ""));
+	},
+	_setOption: function(option, value) {
+		
+		switch(option) {				
+			case "width":
+				this.$input.width(value - this.$button.width());					
+				break;
+			case "height":
+				this.$input.height(value);
+				this.$button.height(this._getButtonHeight());				
+				break;	
+			case "maxHeight":
+				this.$input.autocomplete("widget").css("max-height", value + "px");
+				break;
+			case "buttonClass":				
+				this.$button.removeClass(this.options.buttonClass).addClass(value);
+				break;
+			case "buttonStyle":
+				if(typeof value === 'string') {
+					value = $.parseJSON(value);
+				}					
+				this.$button.css(value);
+				break;
+			case "inputClass":
+				this.$input.removeClass(this.options.inputClass).addClass(value);
+				break;
+			case "inputStyle":
+				if(typeof value === 'string') {
+					value = $.parseJSON(value);
+				}					
+				this.$input.css(value);
+				break;			
+		}
+		
+		$.Widget.prototype._setOption.apply(this, arguments);
+	},
+	disable: function() {
+		$.Widget.prototype.disable.call(this);
+		this.close();
+		this.$input.addClass("ui-state-disabled").attr("disabled", "disabled");
+		this.$button.addClass("ui-state-disabled").button('disable');
+		this._trigger("disable");
+	},
+	enable: function() {
+		$.Widget.prototype.enable.call(this);
+		this.$input.removeClass("ui-state-disabled").removeAttr("disabled");
+		this.$button.removeClass("ui-state-disabled").button('enable');
+		this._trigger("enable");
+	},
+	open: function() {
+		this.$input.autocomplete("search", "").select();
+		this._trigger("open");
+	},
+	close: function() {
+		this.$input.autocomplete("close");
+		this._trigger("close");
 	},
 	destroy: function() {
 		var $select = this.element;
